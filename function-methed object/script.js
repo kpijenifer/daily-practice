98cccc0997e300546c776f3d02e74b3104e84dfd
kpijenifer <jenifermech7@gmail.com>
2022-09-16 11:49:30 +0530

    add-file.

---
+++ function-methed object/script.js
@@ -0,0 +1,23 @@
+ 
+ 
+ 
+ 
+ //Write a function that takes an object with two properties as argument
+// It should return the value of the property with key country
+
+
+
+
+
+let  name={
+	
+	name:"jenifer",
+	age:"22",
+	hoppy:["chess","rose","yellows"],
+	regno:["951217114020"]
+	
+}
+for(let key in name){
+	console.log(key+":"+name[key]);
+}
+ 
