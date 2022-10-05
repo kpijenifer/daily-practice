42ceb1691f62fb6cc7d7b39dce63b47d8073e35b
kpijenifer <jenifermech7@gmail.com>
2022-10-05 19:12:20 +0530

    add-file.

--- multiplae object/script.js
+++ multiplae object/script.js
@@ -41,19 +41,90 @@
 //   document.getElementById("btn").addEventListener("click",colorchange);
 
 
-function colorchange(){
- let r=Math.floor(Math.random()*256);
-  let g=Math.floor(Math.random()*256);
-  let b=Math.floor(Math.random()*256);
-   let cor="rgb("+r+","+g+","+b+")";
-  let head=document.getElementById("heading");
-  head.style.color=cor;
+// function colorchange(){
+//  let r=Math.floor(Math.random()*256);
+//   let g=Math.floor(Math.random()*256);
+//   let b=Math.floor(Math.random()*256);
+//    let cor="rgb("+r+","+g+","+b+")";
+//   let head=document.getElementById("heading");
+// //   head.style.color=cor;
 
-}
+// // }
 
-document.getElementById("btn").addEventListener("click,",colorchange);
+// // document.getElementById("btn").addEventListener("click,",colorchange);
 
 
+
+
+
+
+
+// function colorchange(){
+//     let r=Math.floor(Math.random()*256);
+//   let g=Math.floor(Math.random()*256);
+//     let b=Math.floor(Math.random()*256);
+//   let clr="rgb("+r+","+g+","+b+")";
+
+//     let head=document.getElementById('head');
+//     head.style.color=clr;
+// }
+
+// let btn=document.getElementById('btn');
+// btn.addEventListener('click',colorchange);
+
+
+
+
+
+
+
+// let object=[
+//   {
+// name:"jenifer",
+// age:"22",
+// village:"kurumbalaperi"
+// },
+// {
+//   name:"raja",
+// age:"22",
+// village:"kurumbalaperi"
+// },
+// {
+//   name:"surya",
+// age:"22",
+// village:"kurumbalaperi"
+// },
+// {
+//   name:"ananth",
+// age:"22",
+// village:"kurumbalaperi"
+// },
+// {
+//   name:"ram",
+// age:"22",
+// village:"kurumbalaperi"
+// }
+// ]
+//  let sum=0;
+// for(i=0;i<object.length;i++){
+//   for(let key in object[i]){
+//     sum=sum+1;
+//   }
+// }
+// console.log(sum)
+
+
+
+
+let arrayname=["one","two","three","four","five","six","seven","eight","nine"];
+
+
+
+let s=arrayname.toString()
+  for(i=0;i<s;i++){
+   
+    console.log(i);
+  }
 
 
   
