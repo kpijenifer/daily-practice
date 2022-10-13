84e49fb519dc3017ff368421313c7b53bbc9a3af
kpijenifer <jenifermech7@gmail.com>
2022-10-13 15:13:49 +0530

    add-file.

---
+++ polyntromep-program/script.js
@@ -0,0 +1,106 @@
+// let a=prompt("enter the Text"),test=0;
+// let len=a.length;
+// for(i=0;i<=len/2;i++){
+//    if(a[i]==a[len-1-i]){
+
+//    }else{
+//       test++;
+//    }
+// }
+// if(test==0){
+//    console.log("The given text is palindrome");
+// }else{
+//    console.log("The given text is not a palindrome");
+// }
+
+
+// let a=parseInt(prompt("enter the number"));
+// let sum=0;
+// let len=a.length;
+// for(i=0;i<=len/2;i++){
+//    if(a[i]==a[len-1-i]){
+//    }
+//       else{
+//       sum++;
+//       }
+  
+// }
+// if(sum==0){
+//    console.log("the given number is polyindrom number");
+
+// }else{
+ 
+//    console.log("the number is not polyintrom");
+// }
+
+// let object=
+// [{
+//    name:"jenifer",
+//    age:"22",
+//    street:"kurumbalaperi"
+//    },{
+// name:"jenifer",
+// age:"22",
+// street:"kurumbalaperi"
+// },{
+// name:"jenifer",
+// age:"22",
+// street:"kurumbalaperi"
+// },
+
+// ]
+
+// object.forEach(element=>{
+//    for(let key in element){
+//       console.log(element[key]);
+//    }
+// })
+
+
+//  let a=parseInt(prompt("enter the number"));
+//  if(a%7==0 || a%10==7){
+//    console.log("there is buzz number");
+
+//  }
+//  else{
+//    console.log("there is not buzz number")
+//  }
+
+
+// let u=parseInt(prompt("enter the window sheet"));
+
+// if(u%4==0 || u%4==1){
+//    console.log("there are is window sheet");
+
+// }
+// else{
+   // console.log("there is not window sheet")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
