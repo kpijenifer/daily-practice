05d1721bd72504d01cbffe1ad53fec9776025f7b
kpijenifer <jenifermech7@gmail.com>
2022-10-14 17:44:02 +0530

    add-file.

--- volves number/script.js
+++ volves number/script.js
@@ -1,14 +1,156 @@
-let a=prompt("enter the name");
-  let b=["a","e","i","o","u"];
-  let s=0;
+// let a=prompt("enter the name");
+//   let b=["a","e","i","o","u"];
+//   let s=0;
   
 
-  for(i=0;i<a.length;i++){
-    for(j=0;j<b.length;j++){
-      if(a[i]==b[j]){
-        s++;
-      }
+//   for(i=0;i<a.length;i++){
+//     for(j=0;j<b.length;j++){
+//       if(a[i]==b[j]){
+//         s++;
+//       }
       
-    }
-  }
-  console.log(s);
+//     }
+//   }
+//   console.log(s);
+
+
+
+// let a="jenifer678@gmail.com";
+//     let b=a.replace(/[^2468]/g,"");
+//     let c=b.split("");
+//     let sum=0;
+//     for(let i=0;i<c.length;i++){
+//        sum+=parseInt(c[i])
+//       //  console.log(sum);
+//     }
+//     console.log(sum);
+
+
+
+
+// let s="jenifermech7@gmail.com";
+//  let k=s.replace(/[^a-z]/g,"");
+//  console.log(k);
+
+// let v=s.replace(/[^0-9]/g,"");
+// console.log(v);
+// let c=s.replace(/[^@]/g,"")
+// console.log(c);
+
+
+
+// let k=["zero","one","two","three","four","five","six","seven","eight","nine"];
+
+// let s=parseInt(prompt("enter the number"));
+     
+//     let  sum=0;
+//     for(i=0;i<k;i++){
+//         for(j=0;j<s;j++){
+//          if(s||k){
+//             sum=k[i]==s[i];
+//          }
+
+//         }
+    
+//     }
+//     console.log(sum);
+
+
+
+
+
+
+
+
+
+// let k;
+// let sum=0;
+//     let arrayname=["0","1","2","3","4","5","6","7","8","9"];
+//     let a=parseInt(prompt("enter the name"));
+//     for(i=0;i<arrayname.length;i++){
+//         for(j=0;i<a.length;j++){
+//             if(arrayname[i]==a[j]){
+//              sum=sum+1;
+//             console.log(sum)
+//             }
+//         }
+      
+//     } 
+
+
+
+    // let a;
+    // a=parseInt(prompt("enter the name"))
+    // for(i=0;i<a;i++){
+    //     if(``)
+    // }
+
+
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+     
+    
+      
