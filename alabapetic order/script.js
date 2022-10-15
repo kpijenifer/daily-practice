e588be32e8583bd445ca1e5dcebe5f275a461dad
kpijenifer <jenifermech7@gmail.com>
2022-10-16 00:19:54 +0530

    add-file.

---
+++ alabapetic order/script.js
@@ -0,0 +1,14 @@
+let a=prompt("enter ther number");
+// split methed using for prikathu
+    let len=a.split("");
+//   sort methed using for order 
+    len.sort();
+    console.log(len);
+    // console.log("plase enter the name")
+
+    // for(let element of len){
+    //     console.log(element);
+    // }
+
+ 
+
