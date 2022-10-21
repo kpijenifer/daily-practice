b705f512d0874de1a18253790fadc97505130927
kpijenifer <jenifermech7@gmail.com>
2022-10-21 23:10:58 +0530

    add-file.

---
+++ 3. Given an array of numbers, arrange them in a way that it forms the largest value/script.js
@@ -0,0 +1,24 @@
+
+// // ethum oru methed
+let v=[54, 546, 548, 60];
+let sum='';
+
+  
+for(i=v.length-1;i>=0;i--){
+    sum+=v[i];
+    
+}
+console.log(sum)
+
+
+// ethu oru methed
+let a=[54,546,548,60];
+
+
+let k=a.reverse();
+k=a.join("");
+console.log(k);
+
+
+
+
