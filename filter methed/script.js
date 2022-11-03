da9e4757fbfc7b3d404086ecb5d7f158a2b951a8
kpijenifer <jenifermech7@gmail.com>
2022-11-03 21:40:40 +0530

    add-file.

---
+++ filter methed/script.js
@@ -0,0 +1,25 @@
+// filter methed in odd number
+
+
+// let a=[1,2,3,4,5,6,7,8,,9,10];
+//    console.log( a.filter(funname));
+// function funname(a){
+//      if(a%2==1){
+//        return a;
+//      }
+//     }
+
+
+    // filter methed in divsible 5 and 2
+
+//     let a=[1,2,3,4,5,6,7,8,9,10];
+
+// console.log((a.filter(jeni)));
+//     function jeni(a){
+//         if(a%5==0 || a%2==0){
+//             return a;
+//         }
+//     }
+
+
+let 
