457ad27f0e8f51ff9d240498ce53bc9cea8613c9
kpijenifer <jenifermech7@gmail.com>
2022-11-17 00:08:05 +0530

    add-file

--- program/script.js
+++ program/script.js
@@ -1,22 +1,57 @@
-var a = [2,4,4,5,6,7];
-let len=[];
-let sum=0;
+// var a = [2,4,4,5,6,7];
+// let len=[];
+// let sum=0;
 
-// debugger
-for(i=0;i<a.length;i++){
-    if(!len.includes(a[i])){
-     len.push(a[i]);
-    // console.log(a[i]);
-    }
-    else{
-        // console.log("extra value",a[i]);
-        sum=sum+1;
-    }
+// // debugger
+// for(i=0;i<a.length;i++){
+//     if(!len.includes(a[i])){
+//      len.push(a[i]);
+//     // console.log(a[i]);
+//     }
+//     else{
+//         // console.log("extra value",a[i]);
+//         sum=sum+1;
+//     }
+// }
+// for(let i=0;i<sum;i++){ 
+//     len.push("_");
+// }
+// console.log(len);
+
+ 
+
+
+
+// let nums = [1,3,5,6], target = 5
+
+//      let string="";
+//      debugger
+//  for(i=1;i<nums.length;i++){
+//         for(j=0;j<nums[i];j++){
+//             if(nums[i][j]==5){
+//                 string=string+nums[i][j];
+//             }
+        
+//          }
+//  }
+//  console.log(string);
+
+
+let nums = [1,3,5], target = 8;
+let string="";
+sum=0
+debugger 
+
+for(i=0;i<nums.length;i++){
+   //  for(j=1;j<nums.length;j++){
+        if(nums[i]<8){
+            sum++
+        }
+         }
+if(sum==(nums.length));
+{
+    console.log(sum)
 }
-for(let i=0;i<sum;i++){ 
-    len.push("_");
-}
-console.log(len)
 
 
 
