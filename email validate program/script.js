33039f80ff0401de4240deaf83816bbffa673161
kpijenifer <jenifermech7@gmail.com>
2022-11-18 12:02:52 +0530

    add-file

---
+++ email validate program/script.js
@@ -0,0 +1,40 @@
+// let a="jenifermech7@gmail.com";
+
+// // let v=[a,e,i,o,u];
+// var b =[];
+// for(i=0;i<a.length;i++){
+
+//     if(a[i].match(/[0-9]/g)){
+//         // console.log(a[i]);
+//         b.push(a[i])
+//     }
+//     // for(j=0;i<v.length;j++){
+//     //     if(a[i]==v[j]){
+
+//     //     }
+//     //     console.log()
+//     // }
+// }
+// console.log(b)
+
+
+// // without vowel = /[^a,e,i,o,u]/g
+// // special character only print = /[^a-z,A-Z,0-9]/g
+
+
+
+
+let b=151;
+debugger
+ let k= b.toString();
+  
+
+ for(let i=k.length-1;i>0;i--){
+     if(b==k[i]){
+         console.log("poilndrome");
+     }
+     else{
+         console.log("not polindrome")
+     }
+ }
+
